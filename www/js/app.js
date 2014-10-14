c3f19d77a65bbfedd44e2e97cca8b6ae1d5c8205
micko-zivkovic <milan.at.work@gmail.com>
2014-10-14 10:29:59 +0200

    C2

--- www/js/app.js
+++ www/js/app.js
@@ -23,13 +23,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-  
-  
 
-
-
-
-	
 	.state('start', {
 	    abstract: true,
 	    url: '/start',
@@ -105,53 +99,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 		  } 
 		}
 	})
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	  
   .state('elements',{
 	  url: "/elements",
@@ -159,57 +106,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 	  
   });
   
-  
-  
-  
-
-    /*
-.state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
-    })
-    
-
-
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
-*/
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/start/pocetna');
 });
