7e8873a50bce4b45fdb5643ba2161a20f106f3f2
micko-zivkovic <milan.at.work@gmail.com>
2014-10-14 14:51:35 +0200

    s16

---
+++ www/js/pg_sripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("deviceready", function(){
+   
+	if(navigator.network.connection.type == Connection.NONE){
+		alert('No Network');
+	} else {
+		alert('Yes Network');
+	}
+ 
+});
