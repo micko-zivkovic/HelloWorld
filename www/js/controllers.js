194cc5ff64c1055dedd345ffbd1070ffb20b32b8
micko-zivkovic <milan.at.work@gmail.com>
2014-10-14 21:38:21 +0200

    13

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,14 @@ angular.module('starter.controllers', [])
 .controller('startHome', function($scope){
 	$scope.reset = function() {
 		try {
-			console.log(navigator);
+			navigator.geolocation.getCurrentPosition(
+				function(position) {
+					console.log(position);
+				},
+				function(e) { 
+					console.log("Error retrieving position " + e.code + " " + e.message) 
+				}
+			);
 		} catch(err) {
 			console.log(err.message);
 		}
