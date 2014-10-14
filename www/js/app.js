b89c9afb6feac3dbf0fb2ea8a09798fbeadfdb9a
micko-zivkovic <milan.at.work@gmail.com>
2014-10-14 15:17:27 +0200

    s19

--- www/js/app.js
+++ www/js/app.js
@@ -18,11 +18,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
 	
-	if(navigator.network.connection.type == Connection.NONE) {
-		alert('No Network');
-	} else {
-		alert('Yes Network');
-	}
+	console.log('test');
 	
   });
 })
