94c7d0fc63b378cb10657ad6ec818e9500d34e82
micko-zivkovic <milan.at.work@gmail.com>
2014-10-15 12:05:27 +0200

    das

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,8 @@ angular.module('starter.controllers', [])
 .controller('startHome', function($scope){
 	$scope.reset = function() {
 		try {
-			console.log(navigator);
+			//console.log(navigator);
+			console.log(navigator.hasOwnProperty('geolocation'));
 			/*
 			navigator.geolocation.getCurrentPosition(
 				function(position) {
